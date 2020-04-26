e956c87f4fe1b8fc5b29c0d1cde22741c37c6116
Javier Ledezma <juls0593@gmail.com>
2020-04-26 00:09:45 +0000

    Add required config values

--- config/traccar.php
+++ config/traccar.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-
+    'base_url' => env('TRACCAR_BASE_URL', 'https://www.traccar.org/api')
 ];
--- tests/Unit/TraccarConfigTest.php
+++ tests/Unit/TraccarConfigTest.php
@@ -2,7 +2,33 @@
 
 namespace Javleds\Traccar\Tests\Unit;
 
-class TraccarConfigTest
-{
+use Javleds\Traccar\Tests\BaseTestCase;
 
+class TraccarConfigTest extends BaseTestCase
+{
+    const CONFIG_FILENAME = 'traccar';
+
+    /**
+     * @dataProvider getRequiredValues
+     */
+    public function testConfigShouldHaveRequiredValues(string $requiredValue): void
+    {
+        $this->assertNotNull(config($this->getConfigKey($requiredValue)));
+    }
+
+    private function getConfigKey(string $key): string
+    {
+        return sprintf(
+            '%s.%s',
+            self::CONFIG_FILENAME,
+            $key
+        );
+    }
+
+    public function getRequiredValues(): array
+    {
+        return [
+            ['base_url'],
+        ];
+    }
 }
