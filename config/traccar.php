fe34d8988fec10277a3fc577285631cec128fa05
Javier Ledezma <jledezma@medtrainer.com>
2020-04-27 22:44:28 -0500

    Change default class behavior and using it in the laravel-model way

--- config/traccar.php
+++ config/traccar.php
@@ -3,7 +3,7 @@
 return [
     'base_url' => env('TRACCAR_BASE_URL'),
 
-    'debug_requests' => true,
+    'debug_requests' => false,
 
     'auth' => [
         'username' => env('TRACCAR_USERNAME'),
