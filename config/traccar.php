75744e0c06d80867add6c11e02da0a538d5a95d4
Javier Ledezma <jledezma@medtrainer.com>
2020-04-27 22:39:23 -0500

    Remove default values from config file

--- config/traccar.php
+++ config/traccar.php
@@ -1,5 +1,12 @@
 <?php
 
 return [
-    'base_url' => env('TRACCAR_BASE_URL', 'https://www.traccar.org/api')
+    'base_url' => env('TRACCAR_BASE_URL'),
+
+    'debug_requests' => true,
+
+    'auth' => [
+        'username' => env('TRACCAR_USERNAME'),
+        'password' => env('TRACCAR_PASSWORD'),
+    ]
 ];
